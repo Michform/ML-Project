95cbc37ef8a0a648c7687c86b0741b9574b25921
michform <atolagbeibukun001@gmail.com>
2025-10-06 19:23:52 +0100

    Hyper-parameter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,9 +44,32 @@ class ModelTrainer:
 				"KNeighborsRegressor": KNeighborsRegressor(),
 				"AdaBoostRegressor": AdaBoostRegressor()
 			}
+
+			params={
+                "DecisionTreeRegressor": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
+                },
+                "RandomForestRegressor":{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "GradientBoostingRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "LinearRegression":{},
+                "KNeighborsRegressor":{
+                    'n_neighbors':[5,7,9,11]
+                },
+                "AdaBoostRegressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
 			
 			
-			model_report: dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+			model_report: dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test,
+									    y_test=y_test, models=models, param=params)
 			
 			best_model_score = max(sorted(model_report.values()))
 
